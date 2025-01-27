da5a22e78cb790ae93e662a3a6d0d47df9181f5a
Tonnes <to.smith.eg@gmail.com>
2025-01-27 14:52:39 +0100

    Done Core

--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,56 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        private string _word;
+        public Dictionary<char, int> scoreMap = new Dictionary<char, int>
+        {
+            { 'A', 1 },
+            { 'B', 3 },
+            { 'C', 3 },
+            { 'D', 2 },
+            { 'E', 1 },
+            { 'F', 4 },
+            { 'G', 2 },
+            { 'H', 4 },
+            { 'I', 1 },
+            { 'J', 8 },
+            { 'K', 5 },
+            { 'L', 1 },
+            { 'M', 3 },
+            { 'N', 1 },
+            { 'O', 1 },
+            { 'P', 3 },
+            { 'Q', 10 },
+            { 'R', 1 },
+            { 'S', 1 },
+            { 'T', 1 },
+            { 'U', 1 },
+            { 'V', 4 },
+            { 'W', 4 },
+            { 'X', 8 },
+            { 'Y', 4 },
+            { 'Z', 10 }
+        };
         public Scrabble(string word)
         {            
-            //TODO: do something with the word variable
+            _word = word;
         }
 
         public int score()
         {
-            //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int score = 0;
+            foreach (char letter in this._word)
+            {
+                int letterScore = scoreMap[Char.ToUpper(letter)];
+
+                if (letterScore < 0)
+                {
+                    return 0;
+                }
+                score += letterScore;
+            }
+            
+            return score;
         }
     }
 }
